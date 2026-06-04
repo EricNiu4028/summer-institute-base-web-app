7428d5bd17820d4ce0f314ed179a4c4b6698f0ef
Eric Niu <ericniu.us@gmail.com>
2026-06-04 15:39:50 -0400

    Finish step 3

--- app.rb
+++ app.rb
@@ -17,7 +17,13 @@ class App < Sinatra::Base
   end
 
   def title
-    'Summer Instititue Starter App'
+    'Summer Institute Starter App'
+  end
+
+  def project_dirs
+    Dir.children(projects_root).select do |path|
+      Pathname.new("#{projects_root}/#{path}").directory?
+    end.sort_by(&:to_s)
   end
 
   get '/examples' do
@@ -26,7 +32,39 @@ class App < Sinatra::Base
 
   get '/' do
     logger.info('requsting the index')
-    @flash = session.delete(:flash) || { info: 'Welcome to Summer Institute!' }
     erb(:index)
+  end
+
+  get '/projects/new' do
+    erb(:new_project)
+  end
+
+  post '/projects/new' do
+    logger.info("Trying to create a project with: #{params.inspect}")
+    @flash = { info: "Trying to create a project with: #{params.inspect}" }
+    
+    dirname = params[:name].tr(' ', '_').downcase
+    "#{projects_root}/#{dirname}".tap { |d| FileUtils.mkdir_p(d) }
+
+    redirect(url("/projects/#{dirname}"))
+  end
+
+  get '/projects/:name' do
+    @directory = Pathname.new("#{projects_root}/#{params[:name]}")
+
+    if params[:name] == 'new'
+      erb(:new_project)
+    elsif @directory.directory? && @directory.readable?
+      erb(:show_project)
+    else
+      @flash = {danger: "The project '#{params[:name]}' does not exist"}
+      redirect(url('/'))
+    end
+  end
+
+  private
+
+  def projects_root
+    "#{__dir__}/projects"
   end
 end
